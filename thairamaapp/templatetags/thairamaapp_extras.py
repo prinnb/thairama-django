7c1e6b11bb614ed0549c963be6b05239910adc89
prinnb <prinn_band94@hotmail.com>
2014-08-07 11:40:40 -0700

    edit custom template

---
+++ thairamaapp/templatetags/thairamaapp_extras.py
@@ -0,0 +1,9 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def cleanstring(value):
+    return value.replace('$', '_').replace(' ', '_')
