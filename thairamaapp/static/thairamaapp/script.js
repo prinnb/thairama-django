c99908cf4a3fcda0cb70258f7a06f6023ce347c7
prinnb <prinn_band94@hotmail.com>
2014-08-07 10:57:31 -0700

    edit javascript

--- thairamaapp/static/thairamaapp/script.js
+++ thairamaapp/static/thairamaapp/script.js
@@ -26,6 +26,21 @@ $(document).ready(function(){
             currentPagerPosition:'left'
       }); 
 
+      $(function(){
+03     
+04        $('a[href*=#]').click(function() {
+05        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+06            && location.hostname == this.hostname) {
+07                var $target = $(this.hash);
+08                $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+09                if ($target.length) {
+10                    var targetOffset = $target.offset().top;
+11                    $('html,body').animate({scrollTop: targetOffset}, 800); // change number for scroll speed, higher = slower
+12                    return false;
+13                }
+14            }
+15        });
+16    });
 
 });
 
