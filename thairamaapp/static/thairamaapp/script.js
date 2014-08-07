18ab2b807b9665905f26689853dc9307bee33d2c
prinnb <prinn_band94@hotmail.com>
2014-08-07 01:14:56 -0700

    edit javascript

--- thairamaapp/static/thairamaapp/script.js
+++ thairamaapp/static/thairamaapp/script.js
@@ -26,16 +26,19 @@ $(document).ready(function(){
             currentPagerPosition:'left'
       }); 
 
-      $('a[href^="#"]').on('click',function (e) {
-          e.preventDefault();
 
-          var target = this.hash,
-          $target = $(target);
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
-          $('html, body').stop().animate({
-              'scrollTop': $target.offset().top
-          }, 900, 'swing', function () {
-              window.location.hash = target;
-          });
-      });
+
 });
