1293651779b6ab45ae8cd23c26c58762bca462fb
prinnb <prinn_band94@hotmail.com>
2014-08-07 11:58:55 -0700

    edit custom template

--- thairamaapp/templatetags/thairamaapp_extras.py
+++ thairamaapp/templatetags/thairamaapp_extras.py
@@ -6,4 +6,4 @@ register = template.Library()
 @register.filter
 @stringfilter
 def cleanstring(value):
-    return value.replace('Rice & Noodle $7.95', 'Rice_Noodle_7.95').replace(' ', '_').replace('Entree $7.95', 'Entree_7.95')
+    return value.replace('Rice & Noodle $7.95', 'Rice_Noodle_795').replace('Entree $7.95', 'Entree_795').replace(' ', '_')
