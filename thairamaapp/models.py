d52edfcaee342d57ed8408558b4e75924f47abd5
prinnb <prinn_band94@hotmail.com>
2014-08-16 15:53:50 -0700

    edit sugg

--- thairamaapp/models.py
+++ thairamaapp/models.py
@@ -15,7 +15,7 @@ class Suggestion(models.Model):
 	post_date = models.DateTimeField('date posted', auto_now = True)
 	content = models.TextField()   
 	def __unicode__(self):
-		return u"%s" % self.id
+		return self.name + " | " + self.post_date
 
 class MenuCategory(models.Model):
 	"""
