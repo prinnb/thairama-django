eb249065ed396b74bf8b5a9278af2eff3464589b
prinnb <prinn_band94@hotmail.com>
2015-01-15 23:17:07 -0600

    edit email

--- thairama/settings.py
+++ thairama/settings.py
@@ -41,7 +41,7 @@ ALLOWED_HOSTS = []
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Application definition
 
 INSTALLED_APPS = (
