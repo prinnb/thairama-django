23390787feb080c993f24bce6887c897ed1b8956
prinnb <prinn_band94@hotmail.com>
2021-11-20 22:11:45 +0700

    trying to fix upgrade

--- thairamaapp/forms.py
+++ thairamaapp/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from models import Suggestion
+from .models import Suggestion
 from django.utils.translation import gettext as _
 
 class SuggestionForm(ModelForm):
